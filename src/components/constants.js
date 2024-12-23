30c86ef024f29b1d81e257f5ad73ab0b4ee10250
Web-projektit <jukka.aula@omnia.fi>
2024-12-23 09:36:04 +0200

    API_URL Azureen

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,4 +1,5 @@
-const API_URL = 'http://localhost:5000/reactapi';
+const API_URL = `${window.location.origin}/reactapi`;
+//const API_URL = 'http://localhost:5000/reactapi';
 export const firebaseDatabaseUrl = "https://web-2401-todo-default-rtdb.europe-west1.firebasedatabase.app/items/.json"
 export const firebaseDatabaseUrlItem = "https://web-2401-todo-default-rtdb.europe-west1.firebasedatabase.app/items"
 export const csrfUrl = API_URL + '/getcsrf'
